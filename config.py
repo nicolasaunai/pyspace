05c2f242969ee319643736938f0a75591308c6f3
jeandet alexis <alexis.jeandet@member.fsf.org>
2016-06-27 22:47:31 +0200

    Improved config module:   Fixed issue on mac, mknode needs super user rights   Most of Pylint messages fixed   has_key methode implemented
    Signed-off-by: jeandet alexis <alexis.jeandet@member.fsf.org>

--- config.py
+++ config.py
@@ -1,40 +1,38 @@
-import os, configparser
+import os
+import configparser
 
-config_dir=os.path.expanduser('~')+'/.pyspace/'
-_config_file=config_dir+'/pyspace.ini'
+CONFIG_DIR = os.path.expanduser('~')+'/.pyspace/'
+CONFIG_FILE = CONFIG_DIR+'/pyspace.ini'
 
-if not os.path.exists(config_dir):
-    os.mkdir(config_dir)
-if not os.path.exists(_config_file):
-    os.mknod(_config_file)
+if not os.path.exists(CONFIG_DIR):
+    os.mkdir(CONFIG_DIR)
+if not os.path.exists(CONFIG_FILE):
+    open(CONFIG_FILE, "w").close()
 
 _config = configparser.ConfigParser()
 _config.sections()
-_config.read(_config_file)
+_config.read(CONFIG_FILE)
 
 def save_conf():
-    global _config
-    global _config_file
-    configfile=open(_config_file, 'wt')
+    configfile = open(CONFIG_FILE, 'wt')
     _config.write(configfile)
 
-def get_value(section,key):
-    global _config
+def get_value(section, key):
     if not _config.has_section(section):
-        pyspace._config.add_section(section)
+        _config.add_section(section)
     return _config[section].get(key)
 
-def set_value(section,key,value):
-    global _config
+def set_value(section, key, value):
     if not _config.has_section(section):
-        pyspace._config.add_section(section)
-    _config[section][key]=value
+        _config.add_section(section)
+    _config[section][key] = value
     save_conf()
 
-def has_key(section,key):
-    global _config
+def has_key(section, key):
     if not _config.has_section(section):
-        pyspace._config.add_section(section)
-    _config[section][key]=value
+        return False
+    if _config[section].get(key) is None:
+        return False
+    return True
 
 
