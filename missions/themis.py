1a00fcc73f82de3350c4a776b29ae384933e1bfa
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2016-12-23 19:37:42 +0100

    update themis download routines

--- missions/themis.py
+++ missions/themis.py
@@ -3,6 +3,7 @@
 
 
 import urllib2 as murl
+import urllib
 import os
 import datetime as mdt
 import spacepy.time as mspt
@@ -31,34 +32,35 @@ def themis_dl(sc, instrument, l,date,dest_dir=None):
     file_name = fn #url.split('/')[-1]
 
     if os.path.isfile(file_name):
-        print 'file already exist in the directory'
+        print('file already exist in the directory')
         return None
 
 
-    print 'Downloading %s' % url
+    print("Downloading {}".format(url))
+    urllib.urlretrieve(url, file_name.split('/')[-1])
 
-    u = murl.urlopen(url)
-    f = open(file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-    file_size_dl = 0
-    block_sz = 8192
+#     u = murl.urlopen(url)
+#     f = open(file_name, 'wb')
+#     meta = u.info()
+#     file_size = int(meta.getheaders("Content-Length")[0])
 #
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
+#     print("Downloading: {} Bytes: {}".format(file_name, file_size))
+#
+#     file_size_dl = 0
+#     block_sz = 8192
+# #
+#     while True:
+#         buffer = u.read(block_sz)
+#         if not buffer:
+#             break
+#
+#         file_size_dl += len(buffer)
+#         f.write(buffer)
+#         status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+#         status = status + chr(8)*(len(status)+1)
+#         print(status)
 
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        print status,
-
-    f.close()
+#    f.close()
 #==========================================================
 
 
